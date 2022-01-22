918c72085083d86e71d3877e5a0a626a3c6ebee7
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2022-01-22 16:10:46 +0900

    hack#36: update c++-good

--- h36/c++-good.cpp
+++ h36/c++-good.cpp
@@ -1,12 +1,11 @@
 // c++-good.cpp
 #include "static_mutex.hpp"
-
-extern void do_something() ;
+#include "c++-header.h"
 
 namespace /* anonymous */ { static_mutex m = STATIC_MUTEX_INIT ; }
-void need_to_sync()
+void need_to_sync(int no)
 {
 	static_mutex::scoped_lock lk(m) ;
-		do_something() ;
+		do_something(no) ;
 }
 
